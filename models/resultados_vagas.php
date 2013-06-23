74005fb42c8efdf75ab65fe6f94e45c2b27c32e9
thiagolte <thiagoleite.silva@gmail.com>
2013-06-23 18:52:38 -0300

    Busca de vagas

--- models/resultados_vagas.php
+++ models/resultados_vagas.php
@@ -43,8 +43,7 @@ class Resultados_vagas_Model {
     //SELECT
     public function get_Vagas($categoria, $vaga, $estado, $cidade, $faixa, $pne){
 
-       $Retorno = $this->MySQLSelect(
-                "
+       $query = "
                 SELECT 
                     SHA1(MD5(codigoVAGAEMPRESA)) as idVaga,
                     CONCAT(cidadeempresaVAGAEMPRESA, ' - ', estadoempresaVAGAEMPRESA) AS 'Local',
@@ -59,9 +58,29 @@ class Resultados_vagas_Model {
                     tb0008_Vagas ON codigoVAGA = vagaVAGAEMPRESA
                 LEFT JOIN
                     tb0014_Filtros_Vaga ON vagaempresaFILTROVAGA = codigoVAGAEMPRESA
-                ;
-                "
-        );
+                WHERE codigoVAGAEMPRESA > 0";
+
+                if(!empty($categoria) && isset($categoria)){
+                    $query .= " AND categoriaVAGAEMPRESA LIKE '%$categoria%'";
+                }
+                if(!empty($vaga) && isset($vaga)){
+                    $query .= " AND vagaVAGAEMPRESA LIKE '%$vaga%'";
+                }
+                if(!empty($estado) && isset($estado)){
+                    $query .= " AND estadoempresaVAGAEMPRESA LIKE '%$estado%'";
+                }
+                if(!empty($cidade) && isset($cidade)){
+                    $query .= " AND cidadeempresaVAGAEMPRESA LIKE '%$cidade%'";
+                }
+                if(!empty($faixa) && isset($faixa)){
+                    $query .= " AND	salarioVAGAEMPRESA LIKE '%$faixa%'";
+                }
+//                if(!empty($pne) && isset($pne)){
+//                    $query .= " AND	pneFILTROVAGA LIKE '%$pne%'";
+//                }
+                
+        
+       $Retorno = $this->MySQLSelect($query);
         
         return $Retorno;
     }
