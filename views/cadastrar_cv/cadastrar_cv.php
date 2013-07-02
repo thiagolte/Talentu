e661a3c9a109486f2ca846d10e1f111ed68b9e7a
Jean Reis <jeanpreis@gmail.com>
2013-07-02 00:45:07 -0300

    footer

--- views/cadastrar_cv/cadastrar_cv.php
+++ views/cadastrar_cv/cadastrar_cv.php
@@ -613,11 +613,6 @@
         </div>
     </div>
     
-    <footer>
-        <label>
-			Talentu 2013 © Todos os direitos reservados <span>Desenvolvido por Jean Reis</span><br/>
-			Rua do Tesouro, 23 - 6º andar <span><a href="mailto:contato@talentu.com.br" target="_blank">contato@talentu.com.br</a></span> (11) 2386-2001.
-		</label>
-    </footer>    
+    <?php include 'views/footer.php'; ?>   
 </body>
 </html>
---
+++ views/footer.php
@@ -0,0 +1,8 @@
+
+<footer>
+	<label>
+		Talentu 2013 © Todos os direitos reservados <a href="http://jeanreis.com/portfolio/" target="_blank"><span>Desenvolvido por Jean Reis</span></a><br/>
+		Rua do Tesouro, 23 - 6º andar <span>
+		<a href="mailto:contato@talentu.com.br" target="_blank">contato@talentu.com.br</a></span> (11) 2386-2001.
+	</label>
+</footer>
