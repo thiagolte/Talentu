e1778fe5fb2ee0b55f5d038208e8190be2bb792c
Jean Reis <jeanpreis@gmail.com>
2013-08-14 23:31:22 -0300

    correcoes

--- js/visualizar_vaga/visualizar_vaga.js
+++ js/visualizar_vaga/visualizar_vaga.js
@@ -1,30 +1,52 @@
 $(document).ready(function() {
-    //Enviar Dados CV
 	var opt;
+	var quest1;
+	var quest2;
 	$('.quest').click(function(){
 		opt = 'y';
 	});
+	
     $('#Enviar').click(function(){
-		if(!$('.questTxt').val()) {
-			alert('Por favor, responda a questão');
-		}else if(opt != 'y'){
-			alert('Por favor, escolha entre SIM ou NÃO');
-		}else {		
-			$.ajax({
-				url: "index.php?cadastrar_vaga",
-				type: "GET",
-				data: { ICadidatar: $('#frmVaga').serializeObject() },
-				success: function(data)
-				{
-					if(data > 0){
-							alert('Candidatado com sucesso!');
-					}else{
-							alert('ERRO: Contate o administrador');
-					}
-				}
-			});
+		if($('input[type=radio]').hasClass('quest')){
+			quest1 = 'n';
+			if(opt != 'y'){
+				alert('Por favor, escolha entre SIM ou NÃO');
+			}else {
+				quest1 = 's';
+			}
+		}else {
+			quest1 = 's';
+		}
+		if($('textarea').hasClass('questTxt')){
+			quest1 = 'n';
+			if(!$('.questTxt').val()) {
+				alert('Por favor, responda a questão');
+			} else {
+				quest1 = 's';
+			}	
+		}
+		if(quest1 == 's'){
+			cadastrar();
 		}
     });
+	
+	//Enviar Dados CV 	
+	function cadastrar(){
+		$.ajax({
+			url: "index.php?cadastrar_vaga",
+			type: "GET",
+			data: { ICadidatar: $('#frmVaga').serializeObject() },
+			success: function(data)
+			{
+				if(data > 0){
+						alert('Candidatado com sucesso!');
+				}else{
+						alert('ERRO: Contate o administrador');
+				}
+			}
+		});
+	}
+   
     
     $('#Cadastro').click(function(){
         window.location = "?cadastrar_cv";
